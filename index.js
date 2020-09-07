dbcc10bbbb8d6b2d1a9f00f25016e2d456d894bb
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-07 02:52:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ class Breakfast {
     this.drink = drink;
   }
 }
+
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
